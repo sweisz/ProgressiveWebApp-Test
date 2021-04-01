05427ff12d562775505413e804bbc525e265ed3d
sweisz <dev@steffenweisz.de>
2021-04-01 16:31:40 +0200

    Handle push nozification

--- worker/index.js
+++ worker/index.js
@@ -8,3 +8,24 @@ self.addEventListener("message", (event) => {
   //     window.workbox.messageSW({command: 'log', message: 'hello world'})
   console.log(event.data);
 });
+
+self.addEventListener("push", function (event) {
+  const options = {
+    body: event.data.text(),
+    icon: "images/example.png",
+    vibrate: [100, 50, 100],
+    data: {
+      dateOfArrival: Date.now(),
+      primaryKey: "2",
+    },
+    actions: [
+      {
+        action: "explore",
+        title: "Explore this new world",
+        icon: "images/checkmark.png",
+      },
+      { action: "close", title: "Close", icon: "images/xmark.png" },
+    ],
+  };
+  event.waitUntil(self.registration.showNotification("Hello world!", options));
+});
